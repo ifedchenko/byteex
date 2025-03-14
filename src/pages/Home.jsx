93d3067a8ef6a35bb93432c1d18c7a78f6d70dbe
Yriy Fedchenko <y.fedchenko@gmail.com>
2025-03-14 12:47:05 +0200

    add basic settings

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
+};
+
+export default Home;
